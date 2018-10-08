6aad398a21282d14abf8de4043cc0185eae2249d
tysky <dtysky51@gmail.com>
2018-10-08 13:06:18 +0300

    update channels rendering to react-bootstrap components

--- src/components/ChannelsList.jsx
+++ src/components/ChannelsList.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Col, ListGroup } from 'react-bootstrap';
 import ChatContext from '../context';
 
 // eslint-disable-next-line import/prefer-default-export
 export const ChannelsList = () => (
   <ChatContext.Consumer>
     {channels => (
-      <ul className="list-group">
-        {channels.map(({ name }) => <li key={name} className="list-group-item">{name}</li>)}
-      </ul>
+      <Col sm={3}>
+        <ListGroup>
+          {channels.map(({ name }) => <ListGroup.Item key={name}>{name}</ListGroup.Item>)}
+        </ListGroup>
+      </Col>
     )}
   </ChatContext.Consumer>
 );
